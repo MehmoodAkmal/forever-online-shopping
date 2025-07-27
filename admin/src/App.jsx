17e2048aaadb75e6cf24093127da5ec8d71e732b
mehmood akmal <akmalbajwa2672@gmail.com>
2025-07-27 06:26:14 -0700

    navbar sidebar pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,30 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Order from "./pages/Order";
 
 function App() {
-
-
   return (
-    <>
-      <h1 className='text-2xl text-red-600'>Hello</h1>
-    </>
-      
-  )
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr />
+        <div className="flex w-full">
+          <Sidebar /> 
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path="/add" element={<Add />}/>
+            <Route path="/list" element={<List />}/>
+            <Route path="/order" element={<Order />}/>
+          </Routes>
+          </div>
+        </div>
+      </>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets.js'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-[4%] sm:px-15 py-2'>
+        <img className='w-[25%] sm:w-[15%] md:w-[10%]' src={assets.logo} alt="" />
+        <button className='cursor-pointer text-white bg-gray-600 px-5 py-2 sm:px-7 text-sm sm:py-2 rounded-full'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
